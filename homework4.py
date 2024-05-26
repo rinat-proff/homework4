526eeaf7e4499951b5c12ae57d52f4198eb48229
rinat-proff <rinat-proff@mail.ru>
2024-05-26 23:35:13 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = input('Как Вас зовут: ')
+print(len("Как Вас зовут "))
+print('как вас зовут: ' .upper())
+print('КАК ВАС ЗОВУТ: ' .lower())
+print('Как Вас зовут: ' .replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
